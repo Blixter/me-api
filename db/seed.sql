86b77c17abd73aec8fc754a04631673a00a6ff1b
Robin Blixter <r.blixter89@gmail.com>
2019-09-23 12:37:15 +0200

    Small changes to the reports table

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,6 @@
 INSERT INTO me (name, city, description) VALUES ("Robin Blixter", "Göteborg", "<p>Mitt namn är Robin Blixter. Född och uppvuxen i Göteborg. Jag har alltid varit intresserad av teknik, jag var runt 9 år första gången som vi fick en dator till familjen. Jag fastnade direkt, spelade datorspel framför allt. Vi hade inget bredband på den tiden så det fanns inte så mycket annat att göra. Längre fram när jag fick egna pengar började jag bygga mina egna datorer, främst i syftet att spela på dem.</p><p>Programmering upptäckte jag första gången på gymnasiet när jag läste kursen Programmering A. Då fick jag lära mig att programmera simpla spel i ren HTML-kod. Jag kom ihåg att jag gjorde ett ping pong-spel där man mötte datorn.</p><p>Jag läste alla IT-kurser som jag kunde på gymnasiet, olika omständigheter fick mig att hamna på andra väger. Jag började jobba som tidningsbud och sedan brevbärare, fick problem med knäna på grund av allt spring i trapporna. Kort därefter fick jag barn och började jobba som stödassistent i Lundby här i Göteborg. Jag fick vikariat och behövde utbilda mig för att få fortsätta jobba. Jag behövde en tillsvidare anställning och började plugga till undersköterska med fokus på psykiatri, utbildningen var i 3 terminer. Jag fick en tillsvidare anställning så fort jag var klar med utbildningen.</p><p>Nu några år senare känner jag att jag vill ta tag i mitt IT-intresse igen. Är tjänstledig från mitt jobb och pluggar på heltid.</p><p>Som hobby har jag även fiske, främst fiske i insjöar efter gädda och abborre. Fiskar från flytring eller polarens båt. Min största gädda hittils var 103 cm lång och vägde 7,4 kg, längar tills jag kommer över 10 kilogränsen. Största abborren som jag har fått upp vägde 1,1 kg. Jag och min familj brukar åka uppåt i landet på semester tillsammans med ett kompispar som har barn i liknande åldrar, för att campa och självklart fiska.</p>");
 
-INSERT INTO reports (id, author, text) VALUES ("1", "Robin Blixter", "# Jsramverk
+INSERT INTO reports (id, author, text) VALUES ("1", "# Jsramverk
 
 ## Available Scripts
 
@@ -47,7 +47,7 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 ## Github repo
 Here's a link to the repo on GitHub: [https://github.com/Blixter/jsramverk](https://github.com/Blixter/jsramverk).")
 
-INSERT INTO reports (id, author, text) VALUES ("2", "Robin Blixter", "# Vecka 2
+INSERT INTO reports (id, author, text) VALUES ("2", "# Vecka 2
 
 Vecka 2 och jag har nu arbetat med ett registreringsformulär samt en date-picker.
 Jag valde att göra ungefär som Gmails date-picker för födelsedatum. Det blir det smidigaste sättet tycker jag. Eftersom man först kan välja månad i dropdown-menyn, sedan är det bara att tabba och skriva dag och år. Jag gjorde input-fältet för dagen lite mindre och året lite större, för att användaren skall förstå att året skall skrivas med fler siffror, alltså YYYY. Som input valde jag självklart 'number'. Det finns mer jobb att göra för att den skall stämma helt och hållet. Till exempel så går det att fylla i 1 - 31 dagar för varje månad. Jag har gjort så att det inte blir validerat om användaren skriver utanför 1 - 31 i dagar, samt om man inte håller sig innanför 1900 - nuvarande år.
